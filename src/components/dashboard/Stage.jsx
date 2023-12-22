5ced2f5b8ab6eaab1a5d3fcfba379ca3dd041bca
ismail-hossen <satict2016@gmail.com>
2023-12-22 18:13:45 +0600

    created update form for updating task

--- src/components/dashboard/Stage.jsx
+++ src/components/dashboard/Stage.jsx
@@ -30,7 +30,9 @@ const Stage = ({ stage, tasks, setRefetch }) => {
       <h2 className="text-xl font-semibold">{stage}</h2>
       <div className="grid grid-cols-1 gap-y-4 mt-2">
         {tasks?.length > 0 &&
-          tasks.map((task) => <TaskCard key={task._id} task={task} />)}
+          tasks.map((task) => (
+            <TaskCard key={task._id} setRefetch={setRefetch} task={task} />
+          ))}
       </div>
     </div>
   );
--- src/components/dashboard/TaskCard.jsx
+++ src/components/dashboard/TaskCard.jsx
@@ -1,11 +1,16 @@
+import axios from "axios";
 import { useState } from "react";
 import { useDrag } from "react-dnd";
+import toast from "react-hot-toast";
 import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin2Line } from "react-icons/ri";
+import Modal from "../Modal";
+import UpdateTaskForm from "./UpdateFormTask";
 
-const TaskCard = ({ task }) => {
-  const { title, description } = task || {};
+const TaskCard = ({ task, setRefetch }) => {
+  const { title, description, _id } = task || {};
   const [showActionBtn, setShowActionBtn] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "task",
     item: task,
@@ -13,6 +18,23 @@ const TaskCard = ({ task }) => {
       isDragging: !!monitor.isDragging(),
     }),
   }));
+
+  const handledDelete = (id) => {
+    axios.delete(`http://localhost:4000/delete-task/${id}`).then((res) => {
+      if (res?.data?.deletedCount > 0) {
+        setRefetch(true);
+        toast.success("Delete Success!");
+      }
+    });
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div
@@ -30,10 +52,10 @@ const TaskCard = ({ task }) => {
           </h2>
           {showActionBtn && (
             <div className="flex gap-3">
-              <button className="text-xl">
+              <button onClick={openModal} className="text-xl">
                 <FaRegEdit />
               </button>
-              <button className="text-xl">
+              <button onClick={() => handledDelete(_id)} className="text-xl">
                 <RiDeleteBin2Line />
               </button>
             </div>
@@ -43,6 +65,13 @@ const TaskCard = ({ task }) => {
           {description.length > 80 ? description.slice(0, 80) : description}
         </p>
       </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal} modalTitle="Update Task">
+        <UpdateTaskForm
+          setRefetch={setRefetch}
+          onClose={closeModal}
+          updateTask={task}
+        />
+      </Modal>
     </div>
   );
 };
